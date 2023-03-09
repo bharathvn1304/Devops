b8ae7dd245f772bc1c6f3e82a69f43aa3839c762
Vardharaju Narayana <bharath.vardharaju-narayana@hpe.com>
2023-03-09 12:17:09 -0600

    Autopush

--- 02-dynamic.sh
+++ 02-dynamic.sh
@@ -2,3 +2,4 @@
 
 TODAYDATE=$(date +%F) # This way of declaring is called as hardcoding.
 echo -e "Welcome to Bash Training, Today date is \e[32m ${TODAYDATE} \e[0m"
+cho -e "Number of users sessions in the system are : \e[32m  $(who | wc -l) \e[0m"
