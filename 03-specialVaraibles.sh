f0e7cf230c9dea0656a88cf095613bafa3d07e08
Vardharaju Narayana <bharath.vardharaju-narayana@hpe.com>
2023-03-09 12:38:06 -0600

    Autopush

--- 03-specialVaraibles.sh
+++ 03-specialVaraibles.sh
@@ -6,11 +6,11 @@ a=10
 
 echo "Value of a is $a"
 
-echo "Name of the script is : $0"     # Gives the name of the script you're running  
+echo "Name of the script is : $0"  # Gives the name of the script you're running  
 
 echo first value is $1
 echo second value is $2 
 echo third value is $3
 
 # bash scriptName.sh 100 200 300
-echo "supplied variables are : $"
+echo "supplied variables are : $0"
