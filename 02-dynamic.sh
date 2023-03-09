8b8463a899d5ffebe10baf960893dc03b818405a
Vardharaju Narayana <bharath.vardharaju-narayana@hpe.com>
2023-03-09 12:12:39 -0600

    Autopush

--- 02-dynamic.sh
+++ 02-dynamic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 TODAYDATE=*03-09-2023*  # This way of declaring is called as hardcoding.
-echo -e "Welcome to Bash Training, Today date is ${TODAYDATE}"
+echo -e "Welcome to Bash Training, Today date is \e[32 ${TODAYDATE} \e[0m"
