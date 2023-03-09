814625abc5f638f43f21a4a1fb0168c5857e160c
Vardharaju Narayana <bharath.vardharaju-narayana@hpe.com>
2023-03-09 12:28:36 -0600

    Autopush

--- 03-specialVaraibles.sh
+++ 03-specialVaraibles.sh
@@ -2,4 +2,12 @@
 
 # Special Characters :  $0 to $9 , $* , $# , #@ , $$ 
 
+a=10
+
+echo "Value of a is $a"
+
 echo "Name of the script is : $0"     # Gives the name of the script you're running  
+
+echo first value is $1
+echo second value is $2 
+echo third value is $3
