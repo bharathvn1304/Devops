327eb94e48ecefaf5b7a23bbc1ee03a7380a2ec2
Vardharaju Narayana <bharath.vardharaju-narayana@hpe.com>
2023-03-09 00:07:07 -0600

    Autopush

---
+++ 01-shebang.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+# Start of the line in any bash script should always be a SHEBANG  Notation. 
+
+# Apart from the first line, none of the lines are considers as shebang.
+# And SHEBANG Notation should alwasy start with #
+# SHEBANG : Using this notation, we are going to tell system that the interpretor to be used for this script is bash or python or shell
+# Apart from the line 1 or the first line of the script, anything which starts with `#` will be referred a COMMENT
+# What is COMMENT ? Comment is just to add some desription or to exclude something
+
+# How to run a script ?
+    # bash scriptName 
+    # sh scriptName 
+    # ./scriptName
+
+echo Hello World 
+echo This is first session of bash 
+echo We know how to run a  script
+
+echo line1
+echo line2
+echo line3
+
+# How to print multiple lines using a single echo command ? 
+    # To do so, we need to use escape sequenece characters
